1cc38560258ecbb86c8e1c9025fe2746a23427bc
Lennin666 <soporte@allnexus.com.co>
2021-03-22 16:51:10 -0500

    definiendo un useState para la nueva tarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+//Creamos un type para hacer mas corto el llamado e:FormElement
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <button>Guardar</button>
+      </form>
+    </>
   );
 }
 
