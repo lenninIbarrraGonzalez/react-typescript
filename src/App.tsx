2d191a89d57dc2d7e5bca56ee088d3581c37518b
Lennin666 <soporte@allnexus.com.co>
2021-03-22 17:02:45 -0500

    Agregando una nueva tarea

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,36 @@ import React, { useState } from "react";
 //Creamos un type para hacer mas corto el llamado e:FormElement
 type FormElement = React.FormEvent<HTMLFormElement>;
 
+//Creamos una interface para las tareas [{},{},{}]
+interface ITask {
+  name: string;
+  done: boolean;
+}
+
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
-  
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
+    addTask(newTask);
+    console.log(tasks);
+    setNewTask("");
   };
 
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
         <button>Guardar</button>
       </form>
     </>
